23ceb53e983134b9f13df530654993a8ad60ebb1
mob <innermond@yahoo.com>
2019-03-15 18:48:28 +0200

    layer text with dimensions

--- pack.go
+++ pack.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"math"
 	"sort"
 	"strconv"
@@ -35,7 +36,17 @@ func outsvg(canvas *svg.SVG, blocks []*Node) error {
 	canvas.Group("id=\"blocks\"", "inkscape:label=\"blocks\"", "inkscape:groupmode=\"layer\"")
 	for _, blk := range blocks {
 		if blk.fit != nil {
-			canvas.Rect(blk.fit.x, blk.fit.y, blk.w, blk.h, "fill:none;stroke-width:0.1;stroke-opacity:1;stroke:#000")
+			canvas.Rect(blk.fit.x, blk.fit.y, blk.w, blk.h, "fill:none;stroke-width:0.2;stroke-opacity:1;stroke:#000")
+		} else {
+			return errors.New("unexpected unfit block")
+		}
+	}
+	canvas.Gend()
+
+	canvas.Group("id=\"dimensions\"", "inkscape:label=\"dimensions\"", "inkscape:groupmode=\"layer\"")
+	for _, blk := range blocks {
+		if blk.fit != nil {
+			canvas.Text(blk.fit.x+blk.w/2, blk.fit.y+blk.h/2, fmt.Sprintf("%dx%d", blk.w, blk.h), "text-anchor:middle;font-size:72pt;fill:#000")
 		} else {
 			return errors.New("unexpected unfit block")
 		}
