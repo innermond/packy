24497106f675ad2a148c33864b114068ad044095
mob <innermond@yahoo.com>
2019-03-15 18:10:55 +0200

    fixes+cli

--- main.go
+++ main.go
@@ -1,34 +1,81 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"math"
 	"os"
-	"strconv"
 
 	svg "github.com/ajstarks/svgo"
 )
 
-func main() {
-	dimstr := "100x100x2 20x60x7 50x100 70x130x2 50x50x3 50x100x1 50x50"
-	dims := dimString(dimstr)
-	blocks := blocksArranged(dims)
+var (
+	outname, unit, dimensions string
+	report, output            bool
+)
 
-	width := 200
-	height := 200
-	unfit := blocks[:]
+func initFlag() {
+	flag.StringVar(&outname, "o", "fit", "name of the maching project")
+	flag.StringVar(&unit, "u", "mm", "unit of measurements")
+	flag.BoolVar(&report, "r", true, "match report")
+	flag.BoolVar(&output, "f", false, "outputing files representing matching")
+
+	flag.Parse()
+}
+
+func main() {
+	initFlag()
+	dimensions := flag.Args()
+
+	dims := dimString(dimensions)
+	unfit := blocksArranged(dims)
+	fit := []*Node{}
+
+	width := 2030
+	height := 3050
 	var canvas *svg.SVG
 	unfitlen := len(unfit)
 	inx := 0
+	stats := ""
 	for unfitlen > 0 {
-		f, err := os.Create("fit." + strconv.Itoa(inx) + ".svg")
-		if err != nil {
-			panic("cannot create file")
-		}
 		inx++
-		unfit, canvas = pack(width, height, unfit, f)
-		canvas.End()
+		fit, unfit = pack(width, height, unfit)
+
+		if output {
+			f, err := os.Create(fmt.Sprintf("%s.%d.svg", outname, inx))
+			if err != nil {
+				panic("cannot create file")
+			}
+			canvas = svg.New(f)
+			canvas.Startunit(width, height, unit, fmt.Sprintf("viewBox=\"0 0 %d %d\"", width, height))
+			outsvg(canvas, fit)
+			canvas.End()
+		}
+
+		if report {
+			aria := 0
+			for _, blk := range fit {
+				aria += blk.w * blk.h
+			}
+			percent := math.Round(100 * float64(aria) / float64(width*height))
+			stats += fmt.Sprintf(
+				"%d %s %d%sx%d%s fit %d percent %.2f\n",
+				inx,
+				outname,
+				width, unit,
+				height, unit,
+				len(fit),
+				percent,
+			)
+		}
+
 		if unfitlen == len(unfit) {
 			break
 		}
 		unfitlen = len(unfit)
+	}
+
+	if report {
+		fmt.Print(stats)
 	}
 }
--- pack.go
+++ pack.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"errors"
 	"math"
 	"sort"
 	"strconv"
@@ -15,28 +15,41 @@ type dim struct {
 	n    int
 }
 
-func pack(width int, height int, blocks []*Node, out io.Writer) (unfit []*Node, canvas *svg.SVG) {
+func pack(width int, height int, blocks []*Node) (fit []*Node, unfit []*Node) {
 	packer := NewPacker(width, height)
 	packer.Fit(blocks)
 
-	canvas = svg.New(out)
-	canvas.Start(width, height)
-	canvas.Group("id=\"blocks\"", "inkscape:label=\"blocks\"", "inkscape:groupmode=\"layer\"")
 	for _, blk := range blocks {
 		if blk.fit != nil {
-			canvas.Rect(blk.fit.x, blk.fit.y, blk.w, blk.h, "fill:none;stroke-width:0.1;stroke-opacity:1;stroke:#000")
+			fit = append(fit, blk)
 		} else {
 			unfit = append(unfit, blk)
 		}
 	}
-	canvas.Gend()
-
 	arrange(unfit)
 
 	return
 }
 
+func outsvg(canvas *svg.SVG, blocks []*Node) error {
+	canvas.Group("id=\"blocks\"", "inkscape:label=\"blocks\"", "inkscape:groupmode=\"layer\"")
+	for _, blk := range blocks {
+		if blk.fit != nil {
+			canvas.Rect(blk.fit.x, blk.fit.y, blk.w, blk.h, "fill:none;stroke-width:0.1;stroke-opacity:1;stroke:#000")
+		} else {
+			return errors.New("unexpected unfit block")
+		}
+	}
+	canvas.Gend()
+
+	return nil
+}
+
 func arrange(bb []*Node) {
+	if len(bb) == 0 {
+		return
+	}
+
 	sort.Slice(bb, func(i, j int) bool {
 		a := bb[i]
 		b := bb[j]
@@ -45,19 +58,23 @@ func arrange(bb []*Node) {
 		bw := float64(b.w)
 		bh := float64(b.h)
 
-		if math.Max(bw, bh) < math.Max(aw, ah) {
+		if math.Max(bw, bh) <= math.Max(aw, ah) {
 			return true
 		}
 
-		if math.Min(bw, bh) < math.Min(aw, ah) {
+		if math.Min(bw, bh) <= math.Min(aw, ah) {
 			return true
 		}
 
-		if bh < ah {
+		if bh <= ah {
 			return true
 		}
 
-		return bw < aw
+		if bw <= aw {
+			return true
+		}
+
+		return false
 	})
 }
 
@@ -76,11 +93,10 @@ func blocksArranged(dims []dim) []*Node {
 	return bb
 }
 
-func dimString(dimstr string) (dims []dim) {
-	dimarr := strings.Fields(dimstr)
+func dimString(dimarr []string) (dims []dim) {
 	for _, dd := range dimarr {
 		d := strings.Split(dd, "x")
-		if len(d) < 3 {
+		if len(d) == 2 {
 			d = append(d, "1")
 		}
 		w, err := strconv.ParseFloat(d[0], 64)
